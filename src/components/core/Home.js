7373ddd687c02820d0ffbf39cf755c5259de22d4
Miguel <vhdez.miguel@gmail.com>
2021-11-06 15:40:58 -0500

    last commit

--- src/components/core/Home.js
+++ src/components/core/Home.js
@@ -38,7 +38,18 @@ const Home = () => {
           <img className="image" src={focusvsdiffuse} />
         </div>
       </div>
-      
+      <div class="footer-basic">
+        <footer>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Home</a></li>
+                <li class="list-inline-item"><a href="#">Services</a></li>
+                <li class="list-inline-item"><a href="#">About</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright">FocusedMode © 2021</p>
+        </footer>
+    </div>
     </>
   );
 };
